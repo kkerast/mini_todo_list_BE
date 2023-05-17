2716446018a608476122974bc48524df7b7ccb95
comecomeDev <jinga0908@gmail.com>
2023-05-17 18:21:43 +0900

    add middleware file

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,4 +1,46 @@
 const jwt = require("jsonwebtoken");
-const Autho = require("../schemas/autho");
+const { User } = require("../models");
 
-module.exports = async (req, res, next) => {};
+//private key, public key 확인
+// httponly jwt blacklist
+// 윺효기간 짧게  ,refresh token  rotation 언제나 1회용
+//
+module.exports = async (req, res, next) => {
+  try {
+    const { authorization } = req.cookies;
+    const { tokenType, token } = authorization.split(" ");
+
+    // # 403 Cookie가 존재하지 않을 경우
+    if (!authorization) {
+      return res
+        .status(403)
+        .json({ errorMessage: "로그인이 필요한 기능입니다." });
+    }
+
+    if (tokenType !== "miniPJBearer") {
+      return res
+        .status(401)
+        .json({ errorMessage: "전달된 쿠키에서 오류가 발생하였습니다." });
+    }
+
+    const decodedToken = jwt.verify(token, "miniproject_key_256");
+    const userId = decodedToken.userId;
+
+    const user = await User.findOne({ where: { userId } });
+    if (!user) {
+      res.clearCookie("authorization");
+      return res
+        .status(403)
+        .json({ errorMessage: "전달된 쿠키에서 오류가 발생하였습니다." });
+    }
+
+    res.locals.user = user;
+
+    next();
+  } catch (error) {
+    res.clearCookie("authorization");
+    return res.status(403).json({
+      errorMessage: "전달된 쿠키에서 오류가 발생하였습니다.",
+    });
+  }
+};
