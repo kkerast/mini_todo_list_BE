f74f85737946354cf63e873023f90111609c8485
comecomeDev <jinga0908@gmail.com>
2023-05-17 18:13:59 +0900

    add

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
-const connect = require("./schemas/");
+//const connect = require("./schemas/");
 const { authosRouter, todosRouter } = require("./routes");
-connect();
+//connect();
 
 app.use(express.json());
-
+app.use(cookieParser);
 app.use("/api", [authosRouter, todosRouter]);
 
 app.listen(app.get("port"), () => {
