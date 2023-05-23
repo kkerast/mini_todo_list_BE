2a5d96ce8b8afc565f32c7a34b7fd9b68c1624f1
kkerast <dlwlsgkqkq@naver.com>
2023-05-23 11:43:13 +0900

    timezone 세팅

--- src/db/config.ts
+++ src/db/config.ts
@@ -9,6 +9,7 @@ const connection = new Sequelize({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  timezone: 'Asia/Seoul',
   logging: false,
   models: [Todos, Users],
 });
